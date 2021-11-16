a4b875b07e47167adcf95c483829c0f952c0f020
Cole Kepford <cole.kepford@uleth.ca>
2021-11-15 19:17:57 -0800

    Created DTO's for every entity. Added DTO functionality to controllers. Created DTO converter

--- src/main/java/com/bugtracker/alpha/controllers/IssueController.java
+++ src/main/java/com/bugtracker/alpha/controllers/IssueController.java
@@ -1,10 +1,14 @@
 package com.bugtracker.alpha.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.bugtracker.alpha.dtos.IssueDto;
+import com.bugtracker.alpha.entities.Company;
 import com.bugtracker.alpha.entities.Issue;
 import com.bugtracker.alpha.services.IssueService;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,75 +24,93 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins="http://localhost:3000")
 public class IssueController {
   private final IssueService issueService;
+  private final ModelMapper modelMapper;
 
   @Autowired
-  public IssueController(IssueService issueService) {
+  public IssueController(IssueService issueService, ModelMapper modelMapper) {
     this.issueService = issueService;
+    this.modelMapper = modelMapper;
   }
 
   @GetMapping("/allIssues")
-  public List<Issue> getAllIssues(){
-    return issueService.getAllIssues();
+  public List<IssueDto> getAllIssues(){
+    List<Issue> issues = issueService.getAllIssues();
+    return issues.stream().map(this::convertToDto).collect(Collectors.toList());
   }
 
   @GetMapping("/getIssue/{id}")
-  public Issue getIssue(@PathVariable("id")long id) {
-    return issueService.getIssue(id);
+  public IssueDto getIssue(@PathVariable("id")long id) {
+    return convertToDto(issueService.getIssue(id));
   }
 
   @GetMapping("/getByCompany/{id}")
-  public List<Issue> getIssuesByCompany(@PathVariable("id")long id) {
-    return issueService.findByCompany(id);
+  public List<IssueDto> getIssuesByCompany(@PathVariable("id")long id) {
+    List<Issue> issues = issueService.findByCompany(id);
+    return issues.stream().map(this::convertToDto).collect(Collectors.toList());
   }
 
   @GetMapping("/findByTitle/{title}")
-  public List<Issue> findByTitle(@PathVariable("title")String title) {
-    return issueService.findByTitle(title);
+  public List<IssueDto> findByTitle(@PathVariable("title")String title) {
+    List<Issue> issues = issueService.findByTitle(title);
+    return issues.stream().map(this::convertToDto).collect(Collectors.toList());
   }
 
   @GetMapping("/findByTitleUser/{title}&{id}")
-  public List<Issue> findByTitleForUser(@PathVariable("title")String title, @PathVariable("id")long id) {
-    return issueService.findByTitleForUser(title, id);
+  public List<IssueDto> findByTitleForUser(@PathVariable("title")String title, @PathVariable("id")long id) {
+    List<Issue> issues = issueService.findByTitleForUser(title, id);
+    return issues.stream().map(this::convertToDto).collect(Collectors.toList());
   }
 
   @GetMapping("/findBySeverity/{severity}")
-  public List<Issue> findBySeverity(@PathVariable("severity")String severity) {
-    return issueService.findBySeverity(severity);
+  public List<IssueDto> findBySeverity(@PathVariable("severity")String severity) {
+    List<Issue> issues = issueService.findBySeverity(severity);
+    return issues.stream().map(this::convertToDto).collect(Collectors.toList());
   }
 
   @GetMapping("/findBySeverity/{severity}&{id}")
-  public List<Issue> findBySeverity(@PathVariable("severity")String severity, @PathVariable("id")long id) {
-    return issueService.findBySeverityForUser(severity, id);
+  public List<IssueDto> findBySeverity(@PathVariable("severity")String severity, @PathVariable("id")long id) {
+    List<Issue> issues = issueService.findBySeverityForUser(severity, id);
+    return issues.stream().map(this::convertToDto).collect(Collectors.toList());
   }
 
   @GetMapping("/findByState/{state}")
-  public List<Issue> findByState(@PathVariable("state")String state) {
-    return issueService.findByState(state);
+  public List<IssueDto> findByState(@PathVariable("state")String state) {
+    List<Issue> issues = issueService.findByState(state);
+    return issues.stream().map(this::convertToDto).collect(Collectors.toList());
   }
 
   @GetMapping("/findByState/{state}&{id}")
-  public List<Issue> findByState(@PathVariable("state")String state, @PathVariable("id") long id) {
-    return issueService.findByStateForUser(state, id);
+  public List<IssueDto> findByStateForUser(@PathVariable("state")String state, @PathVariable("id") long id) {
+    List<Issue> issues = issueService.findByStateForUser(state, id);
+    return issues.stream().map(this::convertToDto).collect(Collectors.toList());
   }
 
   @PostMapping(path="/newIssue", consumes="application/json", produces="application/json")
-  public void addIssue(@RequestBody Issue issue) {
-    issueService.addNewIssue(issue);
+  public void addIssue(@RequestBody IssueDto issueDto) {
+    issueService.addNewIssue(convertToEntity(issueDto));
   }
 
-  @PostMapping(path="/assignIssue/{issue_id}&{user_id}")
+  /*@PostMapping(path="/assignIssue/{issue_id}&{user_id}")
   public void assignIssue(@PathVariable("issue_id")long issue_id, @PathVariable("user_id")long user_id) {
     issueService.assignUserToIssue(issue_id, user_id);
-  }
+  }*/
 
   @PutMapping(path="/deleteIssue")
-  public void deleteIssue(@RequestBody Issue issue) {
-    issueService.deleteIssue(issue);
+  public void deleteIssue(@RequestBody IssueDto issueDto) {
+    issueService.deleteIssue(convertToEntity(issueDto));
   }
 
   @PutMapping(path="/updateIssue")
-  public void updateIssue(@RequestBody Issue issue) {
-    issueService.updateIssue(issue);
+  public void updateIssue(@RequestBody IssueDto issueDto) {
+    issueService.updateIssue(convertToEntity(issueDto));
   }
   
+  private IssueDto convertToDto(Issue issue) {
+    return new IssueDto(issue);
+  }
+
+  private Issue convertToEntity(IssueDto issueDto) {
+    Company company = new Company(issueDto.getCompanyDto().getCompanyId(), issueDto.getCompanyDto().getName());
+    return new IssueDto(issueDto.getIssueId(), issueDto.getTitle(), issueDto.getDescription(), issueDto.getSeverity(), company, issueDto.getType(), issueDto.getState(),
+  }
 }
